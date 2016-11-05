3b1d8a8f278bf0e7bc4f3b6b3ed53628ebacb858
Daan Smits <Daan Smits>
2016-11-05 12:20:09 -0400

    Initial try to get database on website - failed

--- web.py
+++ web.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template, request
+from flask import *
+import sqlite3
+
+DATABASE = 'movies.db'
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	g.db = connect_db()
+	cur = g.db.execute('select score')
+	movies = [dict(score=row[0]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('index.html',movies=movies)
 
 app.run(debug=True)
